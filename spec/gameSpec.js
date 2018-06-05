f159135ccdcce85121d38aae26ea3fc61803ee00
Alexy <alexandra.fina@yahoo.com>
2018-06-05 12:38:34 +0100

    Test pass - Game ends if no one won

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -25,7 +25,21 @@ describe('Tic Tac Toe', function (){
       addOption(4)
       addOption(2)
       expect(game.won).toEqual(true)
-    })
+  })
+
+  it('Game ends if no one won', function() {
+      resetValues()
+      addOption(0)
+      addOption(2)
+      addOption(1)
+      addOption(3)
+      addOption(5)
+      addOption(4)
+      addOption(6)
+      addOption(7)
+      addOption(8)
+      expect(game.won).toEqual(false)
+   })
 
   })
 })
