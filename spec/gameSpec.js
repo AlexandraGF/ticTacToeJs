d004cf9a3a7fd9d12a79f0b47858a64fc3953d83
Alexy <alexandra.fina@yahoo.com>
2018-06-05 12:36:25 +0100

    Test pass - when user clicks, options is shown on the grid

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -4,5 +4,13 @@ describe('Tic Tac Toe', function (){
     it('starts with an empty totalScore', function() {
       expect(game.totalScore).toEqual(0)
     })
+
+  it('After user clicks, the option should be in the specific location on the grid', function() {
+      addOption(0)
+      expect(game.arrayOptions[0]).toEqual('x')
+    })
+
+    
+
   })
 })
--- src/game.js
+++ src/game.js
@@ -4,3 +4,35 @@ const game = {
   arrayOptions: ['', '', '', '', '', '', '', '', ''],
   won: false
 }
+
+let addOption = function (position) {
+  changeChar()
+  game.totalScore += 1
+  game.arrayOptions.splice(position, 1, game.option)
+  checkAllEquals()
+}
+
+let changeChar = function () {
+  if (game.totalScore % 2 === 0) {
+    game.option = 'x'
+  } else {
+    game.option = 'o'
+  }
+}
+
+let checkAllEquals = function () {
+  checkEqual(0, 1, 2, game.option)
+  checkEqual(3, 4, 5, game.option)
+  checkEqual(6, 7, 8, game.option)
+  checkEqual(0, 3, 6, game.option)
+  checkEqual(1, 4, 7, game.option)
+  checkEqual(2, 5, 8, game.option)
+  checkEqual(2, 4, 6, game.option)
+  checkEqual(0, 4, 8, game.option)
+}
+
+let checkEqual = function (a, b, c, option) {
+  if (game.arrayOptions[a] == option && game.arrayOptions[b] == option && game.arrayOptions[c] == option) {
+      return game.won = true
+  }
+}
